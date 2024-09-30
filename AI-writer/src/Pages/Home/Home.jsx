90a971bac2489c180498164ab65766bec720057d
richurichu <paulsonrichu@gmail.com>
2024-09-30 14:40:02 +0530

    new commit

--- AI-writer/src/Pages/Home/Home.jsx
+++ AI-writer/src/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import { loginSuccess, setLogout } from '../../Redux/Slices/AuthSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import { ResetArticleGenerator } from '../../Utils/Helperfunctions'
-
+import CardComponent from '../../Components/CardComponent';
 
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import ArticleCard from '../../Components/ArticleCard'
@@ -97,15 +97,15 @@ function Home() {
 
           <div className="w-full h-screen px-6 py-10 md:px-8 xl:px-12 lg:w-10/12">
 
-            <div className="flex justify-between w-full px-2 py-4 md:py-6 md:px-4 xl:py-8 bg-custom-light-orange rounded-xl ">
+            <div className="flex justify-between w-full px-2 py-4 md:py-6 md:px-4 xl:py-8 bg-custom-dark rounded-xl ">
 
               <div className="flex items-center justify-center space-x-3 lg:hidden ">
                 <IoMenuOutline onClick={() => setIsSidebarVisible(!IsSidebarVisible)} className='text-2xl md:text-3xl' />
-                <h4 className="text-base md:text-xl xl:text-2xl "> Welcome MakTal</h4>
+                <h4 className="text-base text-white md:text-xl xl:text-2xl "> Welcome MakTal</h4>
               </div>
 
-              <h4 className="text-xl xl:text-2xl max-lg:hidden "> Welcome MakTal</h4>
-              <button className="px-3 py-2 text-sm font-semibold md:px-6 rounded-3xl text-custom-black-text hover:bg-hover-button-color bg-custom-dark-orange">Plan Details</button>
+              <h4 className="pl-4 text-xl text-white xl:text-2xl max-lg:hidden "> Welcome MakTal</h4>
+              <button className="px-3 py-2 text-sm font-semibold text-white md:px-6 rounded-3xl hover:bg-hover-button-color bg-custom-dark-orange">Plan Details</button>
             </div>
 
 
@@ -136,6 +136,37 @@ function Home() {
                 FunctionToCall={ResetArticleGenerator}
                 footer={<span className="font-semibold">Unlimited usage</span>}
               />
+
+              {/* <CardComponent
+                title='Article Writer '
+                description={[
+                  'Our most powerful Article Writer where you can plan entire structure of the content.'
+                ]}
+                buttonText='START WRITING'
+                LinkTo='/quick-article-generation'
+              // FunctionToCall={ResetArticleGenerator}
+              />
+
+              <CardComponent
+                title='Article Rewriter '
+                description={[
+                  'Choose a topic and headline and our tool will do the remaining.',
+                  'Recommended if you need only limited control over the content',
+                ]}
+                buttonText='REWRITE NOW'
+                LinkTo='/quick-article-generation'
+              // FunctionToCall={ResetArticleGenerator}
+              />
+
+              <CardComponent
+                title='Article Writer '
+                description={[
+                  'Our most powerful Article Writer where you can plan entire structure of the content'
+                ]}
+                buttonText='CHECK NOW'
+                LinkTo='/quick-article-generation'
+              // FunctionToCall={ResetArticleGenerator}
+              /> */}
             </div>
 
           </div>
