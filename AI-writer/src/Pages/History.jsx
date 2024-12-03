5ae1b18920a6009d4d9e06aa69350bf3a61ba9e3
richurichu <paulsonrichu@gmail.com>
2024-12-03 14:56:51 +0530

    files added

--- AI-writer/src/Pages/History.jsx
+++ AI-writer/src/Pages/History.jsx
@@ -8,7 +8,6 @@ import PlagiarismHistory from '../Components/PlagiarismHistory';
 import { useDispatch, useSelector } from 'react-redux';
 import SessionExpiredPopup from '../Components/ArticleGenerationComponents/SmallComponents/SessionExpiredPopup';
 
-
 function History() {
     const { HistoryTab } = useSelector(state => state.Navigation);
     const { IsSessionExpired } = useSelector((state) => state.Navigation);
@@ -16,11 +15,12 @@ function History() {
 
     return (
         <>
-            <div className="flex justify-center h-full font-poppins ">
+           
+
+           <div className="flex justify-center h-full font-poppins ">
                 <div className=" 2xl:w-2/12 lg:w-3/12 max-lg:hidden">
                     <Sidebar setIsProfilePopup={setIsProfilePopup} />
                 </div>
-        
                 {HistoryTab === 'Article' && <ArticleHistory/>  }
                 {HistoryTab === 'Plagiarism' && <PlagiarismHistory/>  }
 
